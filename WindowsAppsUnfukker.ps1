7eb7e4da40682384228cc6aef0075a1ccbd75742
AgentRev <a456197@gmail.com>
2021-11-12 01:40:19 -0500

    Small tweaks

--- WindowsAppsUnfukker.ps1
+++ WindowsAppsUnfukker.ps1
@@ -11,13 +11,15 @@
 # 3. Execute the script: psexec.exe -s -i powershell -ExecutionPolicy Bypass -File "FULL_PATH_HERE\WindowsAppsUnfukker.ps1"
 #
 # Ajust the bitch-ass variables below as needed.
-# You can include secondary drives as well. For example:
-# $WinAppsPaths = @("C:\Program Files\WindowsApps", "D:\WindowsApps")
+# You can include secondary drives as well. For example: @("C:\Program Files\WindowsApps", "D:\WindowsApps")
+#
+# If you want to restore a backup, you must first open PowerShell with: psexec.exe -s -i powershell
+# Then, for example: icacls "C:\Program Files" /restore "C:\Program Files\WindowsApps_20211109_221014.txt" /c /q 2>$null
 #
 
 $WinAppsPaths = @("C:\Program Files\WindowsApps")
-$BackupExistingPerms = 1  # set to 1 to grab backups before the unfukking
-$DeepResetInheritance = 0  # set to 1 if you disabled inheritance on the entire subfolder tree like dumbass me
+$BackupExistingPerms = 1  # set to 1 to grab a backup before the unfukking
+$DeepFixInheritance = 0  # set to 1 if you disabled inheritance on the entire folder tree like dumbass me
 
 ####################################################################################################
 
@@ -41,7 +43,7 @@ if ([Security.Principal.WindowsIdentity]::GetCurrent().Name -ne 'NT AUTHORITY\SY
 	exit 1
 }
 
-$WinAppsPaths = $WinAppsPaths | ForEach-Object { [Environment]::ExpandEnvironmentVariables($_).TrimEnd("\") }
+$WinAppsPaths = $WinAppsPaths | ForEach-Object { [Environment]::ExpandEnvironmentVariables($_).TrimEnd('\') }
 
 foreach ($WinAppsPath in $WinAppsPaths)
 {
@@ -58,16 +60,15 @@ foreach ($WinAppsPath in $WinAppsPaths)
 if ($BackupExistingPerms)
 {
 	Write-Host
-	Write-Host "Saving backups of existing permissions..." -ForegroundColor Cyan
+	Write-Host "Backup of existing permissions..." -ForegroundColor Cyan
 
 	$Timestamp = Get-Date -Format 'yyyyMMdd_HHmmss'
 
 	foreach ($WinAppsPath in $WinAppsPaths)
 	{
-		icacls $WinAppsPath /save "$WinAppsPath\..\WindowsApps_$Timestamp.txt" /t /c /q 2>$null  # mutes junction errors
-
-		# You can restore backups using the command below. To run it, you must first open PowerShell with: psexec.exe -s -i powershell
-		# icacls "C:\Program Files" /restore "C:\Program Files\WindowsApps_20211109_221014.txt" /c /q 2>$null
+		$BackupPath = Join-Path (Resolve-Path "$WinAppsPath\..").Path "WindowsApps_$Timestamp.txt"
+		icacls $WinAppsPath /save $BackupPath /t /c /q 2>$null  # mutes junction errors
+		Write-Host "Saved to $BackupPath"
 	}
 }
 
@@ -75,14 +76,14 @@ foreach ($WinAppsPath in $WinAppsPaths)
 {
 	Write-Host
 
-	if ($DeepResetInheritance)
+	if ($DeepFixInheritance)
 	{
-		Write-Host "Resetting subfolder tree inheritance, this could take a couple minutes..." -ForegroundColor Cyan
+		Write-Host "Fixing folder tree inheritance, this could take a couple minutes..." -ForegroundColor Cyan
 		icacls "$WinAppsPath\*" /inheritancelevel:e /t /c /q 2>$null  # mutes junction errors
 	}
 	else
 	{
-		Write-Host "Resetting subfolder inheritance..." -ForegroundColor Cyan
+		Write-Host "Fixing subfolders inheritance..." -ForegroundColor Cyan
 		icacls "$WinAppsPath\*" /inheritancelevel:e /c /q 2>$null  # mutes junction errors
 	}
 
@@ -105,7 +106,7 @@ foreach ($WinAppsPath in $WinAppsPaths)
 			if ($AppFolder.Name -Match '(.+?)_.+(_.+)')
 			{
 				$AppFolderPath = $AppFolder.FullName
-				$AppFolderACL = Get-Acl -Path $AppFolderPath
+				$AppFolderACL = Get-Acl $AppFolderPath
 
 				# Grant Users "Read & execute" for this folder, subfolders and files with Microsoft's bullshit condition
 				$MsBullshit = '(XA;OICI;0x1200a9;;;BU;(WIN://SYSAPPID Contains "{0}{1}"))' -f $Matches.1, $Matches.2
@@ -115,7 +116,7 @@ foreach ($WinAppsPath in $WinAppsPaths)
 					Write-Host "Fixing $AppFolderPath"
 					$AppFolderSDDL = $FirstParenthesis.Replace($AppFolderACL.Sddl, "$MsBullshit(", 1)
 					$AppFolderACL.SetSecurityDescriptorSddlForm($AppFolderSDDL)
-					Set-Acl -Path $AppFolderPath -AclObject $AppFolderACL
+					Set-Acl $AppFolderPath $AppFolderACL
 				}
 			}
 		}
@@ -124,14 +125,11 @@ foreach ($WinAppsPath in $WinAppsPaths)
 	Write-Host
 	Write-Host "Fixing WindowsApps permissions..." -ForegroundColor Cyan
 
-	# Default permissions, courtesy of https://www.winhelponline.com/blog/windowsapps-folder-restore-default-permissions/
-	$WinAppsDefaultPerms = 'D:PAI(A;;FA;;;S-1-5-80-956008885-3418522649-1831038044-1853292631-2271478464)(A;OICIIO;GA;;;S-1-5-80-956008885-3418522649-1831038044-1853292631-2271478464)(A;;0x1200a9;;;S-1-15-3-1024-3635283841-2530182609-996808640-1887759898-3848208603-3313616867-983405619-2501854204)(A;OICIIO;GXGR;;;S-1-15-3-1024-3635283841-2530182609-996808640-1887759898-3848208603-3313616867-983405619-2501854204)(A;;FA;;;SY)(A;OICIIO;GA;;;SY)(A;CI;0x1200a9;;;BA)(A;OICI;0x1200a9;;;LS)(A;OICI;0x1200a9;;;NS)(A;OICI;0x1200a9;;;RC)(XA;;0x1200a9;;;BU;(Exists WIN://SYSAPPID))'
+	# Default ownership and permissions, courtesy of https://www.winhelponline.com/blog/windowsapps-folder-restore-default-permissions/
+	$WinAppsDefaultPerms = 'O:S-1-5-80-956008885-3418522649-1831038044-1853292631-2271478464G:SYD:PAI(A;;FA;;;S-1-5-80-956008885-3418522649-1831038044-1853292631-2271478464)(A;OICIIO;GA;;;S-1-5-80-956008885-3418522649-1831038044-1853292631-2271478464)(A;;0x1200a9;;;S-1-15-3-1024-3635283841-2530182609-996808640-1887759898-3848208603-3313616867-983405619-2501854204)(A;OICIIO;GXGR;;;S-1-15-3-1024-3635283841-2530182609-996808640-1887759898-3848208603-3313616867-983405619-2501854204)(A;;FA;;;SY)(A;OICIIO;GA;;;SY)(A;CI;0x1200a9;;;BA)(A;OICI;0x1200a9;;;LS)(A;OICI;0x1200a9;;;NS)(A;OICI;0x1200a9;;;RC)(XA;;0x1200a9;;;BU;(Exists WIN://SYSAPPID))'
 
-	$WinAppsACL = Get-Acl -Path $WinAppsPath
+	$WinAppsACL = Get-Acl $WinAppsPath
 	$WinAppsACL.SetSecurityDescriptorSddlForm($WinAppsDefaultPerms)
-
-	# Restore default ownership
-	$WinAppsACL.SetOwner((New-Object Security.Principal.NTAccount('NT Service\TrustedInstaller')))
 
 	# Grant ALL APPLICATION PACKAGES "Read & execute" for this folder, subfolders and files
 	$GroupID = New-Object Security.Principal.SecurityIdentifier('S-1-15-2-1')
@@ -149,7 +147,7 @@ foreach ($WinAppsPath in $WinAppsPaths)
 	$WinAppsACL.AddAccessRule($NewRule)
 
 	# Apply all of the above
-	Set-Acl -Path $WinAppsPath -AclObject $WinAppsACL
+	Set-Acl $WinAppsPath $WinAppsACL
 
 	$WpSystem = Join-Path $WinAppsPath "..\WpSystem"
 
@@ -159,11 +157,11 @@ foreach ($WinAppsPath in $WinAppsPaths)
 		Write-Host
 		Write-Host "Fixing WpSystem permissions..." -ForegroundColor Cyan
 
-		$WpSystemACL = Get-Acl -Path $WpSystem
+		$WpSystemACL = Get-Acl $WpSystem
 
 		# Grant Users "List folder contents" for this folder only
 		$GroupID = New-Object Security.Principal.SecurityIdentifier('S-1-5-32-545')
-		$NewRule = New-Object Security.AccessControl.FileSystemAccessRule($GroupID, 'ReadAndExecute', 'Allow')
+		$NewRule = New-Object Security.AccessControl.FileSystemAccessRule($GroupID, 'ReadAndExecute', 'None', 'None', 'Allow')
 		$WpSystemACL.AddAccessRule($NewRule)
 
 		# Grant Administrators "Full control" for this folder, subfolders and files
@@ -172,7 +170,7 @@ foreach ($WinAppsPath in $WinAppsPaths)
 		$WpSystemACL.AddAccessRule($NewRule)
 
 		# Apply all of the above
-		Set-Acl -Path $WpSystem -AclObject $WpSystemACL
+		Set-Acl $WpSystem $WpSystemACL
 	}
 }
 
